4cee965e664313faea4f09d38770a4b19cb8e35c
CheziNut <chezinut@gmail.com>
2025-06-19 06:18:31 +0300

    optimize

--- init.js
+++ init.js
@@ -1,44 +1,21 @@
 /*
-* Copyright (c) Codiad & Andr3as, distributed
+* Copyright (c) Codiad & Andr3as, CheziNut distributed
 * as-is and without warranty under the MIT License.
 * See [root]/license.md for more information. This information must remain intact.
 */
 
 (function(global) {
-    var atheos = global.atheos,
-        scripts = document.getElementsByTagName('script'),
-        path = scripts[scripts.length - 1].src.split('?')[0],
-        curpath = path.split('/').slice(0, -1).join('/') + '/';
-
-    // Script loader helper
-    function loadScript(url, callback) {
-        var script = document.createElement('script');
-        script.src = url;
-        script.onload = callback;
-        document.head.appendChild(script);
-    }
-
-    // DOM ready handler
-    document.addEventListener('DOMContentLoaded', function() {
-        atheos.Emmet.init();
-    });
-
+    const atheos = global.atheos;
     atheos.Emmet = {
-        path: curpath,
-        bindKeys: null,
-        
-        init: function() {
-            var _this = this;
-            
+        path: atheos.path + 'plugins/Atheos-Emmet/',
+        init() {
             // Load emmet extension
-            loadScript("components/editor/ace-editor/ext-emmet.js", function() {
-                var Emmet = ace.require("ace/ext/emmet");
-                
+            atheos.common.loadScript(atheos.path + 'components/editor/ace-editor/ext-emmet.js', () => {
+                const Emmet = ace.require('ace/ext/emmet');
                 // Load emmet core
-                loadScript(_this.path + "emmet.js", function() {
-                    Emmet.setCore(window.emmet);
-                });
+                atheos.common.loadScript(this.path + 'emmet.js', () => Emmet.setCore(window.emmet));
             });
         }
     };
+    document.addEventListener('DOMContentLoaded', () => atheos.Emmet.init());
 })(this);
